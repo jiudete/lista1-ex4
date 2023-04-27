c143247431770915ef622f187e4297f5b5a29e0c
jiudete <130181033+jiudete@users.noreply.github.com>
2023-04-26 23:01:50 -0300

    Add files via upload

---
+++ Lista1-Ex_4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int b;
+            int h;
+            Console.Write("Informe o valor da base do triângulo: "); 
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Informe o valor da altura do triângulo: ");
+            h = int.Parse(Console.ReadLine());
+            A = (b * h) / 2;
+            Console.WriteLine("o valor da sua área é {0} ", A);
+            
+        }
+    }
+}
